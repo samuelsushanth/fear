4c57b26fa057b8828c2356ad5636e0fe6d41b2d3
sasanksaisujanadapa <sasanklsujan.93@gmail.com>
2016-08-25 14:06:48 +0200

    core.user variables intialized in a new class !!

---
+++ backend/src/main/java/se/example2/softhouse/core/User.java
@@ -0,0 +1,80 @@
+package se.example2.softhouse.core;
+
+/**
+ * Created by sasanksaisujanadapa on 25/08/16.
+ */
+
+public class User {
+        private Integer id;
+        private String name;
+        private String email;
+        private String password;
+        private Integer score;
+
+        public User() {
+
+        }
+
+        public User(String name, String email, Integer score) {
+            super();
+            this.name = name;
+            this.email = email;
+            this.score = score;
+        }
+
+        public User(String name, String email, String password) {
+            super();
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.score = 0;
+        }
+
+        public User(String name, String email) {
+            super();
+            this.name = name;
+            this.email = email;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public Integer getScore() {
+            return score;
+        }
+
+        public void setScore(Integer score) {
+            this.score = score;
+        }
+
+    }
+
