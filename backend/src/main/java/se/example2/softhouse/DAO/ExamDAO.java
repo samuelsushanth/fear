e9a0f9426c454dc04eab31a9b6669cfe58d2eedb
ypsricharan <ypcharan3@gmail.com>
2016-09-06 17:45:40 +0200

    database

--- backend/src/main/java/se/example2/softhouse/DAO/ExamDAO.java
+++ backend/src/main/java/se/example2/softhouse/DAO/ExamDAO.java
@@ -1,7 +1,40 @@
 package se.example2.softhouse.DAO;
 
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+import se.example2.softhouse.core.Exam;
+import se.example2.softhouse.core.Question;
+
+import java.util.List;
+
 /**
  * Created by charan on 9/6/2016.
  */
+import java.util.List;
+
+@RegisterMapperFactory(BeanMapperFactory.class)
 public interface ExamDAO {
+
+    @SqlUpdate("create table if not exists Exam (id Long auto_increment primary key, text varchar(2000))")
+    void createExamTable();
+
+    @SqlUpdate("delete table Exam")
+    void deleteExamTable();
+
+    @SqlQuery("select * from Exam")
+    List<Exam> list();
+
+    @SqlUpdate("insert into Exam (text) values (:text)")
+    void insQues(@BindBean Exam exam);
+
+    @SqlUpdate("delete from Exam where (id)=(:id)")
+    void delQues(@BindBean Exam exam);
+
+    @SqlUpdate("update QUESTIONS set (text)=(:text) where (id)=(:id)")
+    void updQues(@BindBean Exam exam);
+
+
 }
--- backend/src/main/java/se/example2/softhouse/DAO/QuestionAnswerDao.java
+++ backend/src/main/java/se/example2/softhouse/DAO/QuestionAnswerDao.java
@@ -3,5 +3,41 @@ package se.example2.softhouse.DAO;
 /**
  * Created by charan on 9/6/2016.
  */
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+import se.example2.softhouse.core.Choice;
+import se.example2.softhouse.core.ExamQuestion;
+import se.example2.softhouse.core.Question;
+import se.example2.softhouse.core.QuestionAnswer;
+
+import java.util.List;
+@RegisterMapperFactory(BeanMapperFactory.class)
 public interface QuestionAnswerDao {
+    @SqlUpdate("create table if not exists QuestionAnswer (id Long auto_increment primary key,examId Long,questionId Long)")
+    void createQuestionAnswerTable();
+
+    @SqlUpdate("delete table QuestionAnswer")
+    void deleteQuestionAnswerTable();
+
+    @SqlQuery("select * from QuestionAnswer")
+    List<QuestionAnswer> list();
+
+    @SqlUpdate("insert into QuestionAnswer (questionId,correctChoiceId) values (:questionId, :correctChoiceId)")
+    void insQuestionAnswer(@BindBean QuestionAnswer questionAnswer);
+
+    @SqlUpdate("delete from QuestionAnswer where (questionId)=(:questionId)")
+    void delQuestionAnswer(@BindBean QuestionAnswer questionAnswer);
+
+    @SqlUpdate("update QuestionAnswer set (questionId,,correctChoiceId)=(:questionId, :correctChoiceId) where (id)=(:id)")
+    void updQuestionAnswer(@BindBean QuestionAnswer questionAnswer);
+    //get corresponding exams questions
+    @SqlQuery("select * from QuestionAnswer where (questionId)=(:questionId) ")
+    List<QuestionAnswer> listQuestions(@BindBean QuestionAnswer questionAnswer);
+
+
+
 }
