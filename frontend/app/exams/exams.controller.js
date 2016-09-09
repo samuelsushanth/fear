1db33872a0d16ab3cfadaa5255136e6527887192
siva reddy <siva.agent@gmail.com>
2016-09-09 11:56:42 +0200

    new

--- frontend/app/exams/exams.controller.js
+++ frontend/app/exams/exams.controller.js
@@ -1,3 +1,20 @@
 function ExamsController() {
-    var
+    var vm = this;
+
+
+    vm.$onInit = $onInit;
+    vm.newExam = newExam;
+
+    function $onInit() {
+       vm.examlist=[];
+        //call list of exams from backend and store in examlist
+
+    }
+
+    //to create new exam
+    function newExam()
+    {
+        //choiceService.update(vm.question, vm.choices, vm.answer).then(vm.refreshF())
+    }
+
 }
