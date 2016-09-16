7daf0100f86711ebf4149f2154d27126bfeca3d8
ratna8 <pranathi.r8@gmail.com>
2016-09-16 11:41:38 +0200

    new

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,39 +3,7 @@ angular.module('app', ["ngRoute"])
     .factory("choiceService", choiceService)
     .factory("examsService", examsService)
     .config(config)
-    .component('noteForm', {
-        templateUrl: 'app/note-form/note-form.tpl',
-        controller: NoteFormController,
-        controllerAs: 'vm',
 
-        require: {
-            notesController: '^notes'
-        },
-
-        bindings: {
-            data: '<',
-            onSubmit: '<',
-            onReset: '<'
-        }
-    })
-    .component('notes', {
-        templateUrl: 'app/notes/notes.tpl',
-        controller: NotesController,
-        controllerAs: 'vm'
-    })
-    .component('note', {
-        templateUrl: 'app/note/note.tpl',
-        controller: NoteController,
-        controllerAs: 'vm',
-
-        require: {
-            notesController: '^notes'
-        },
-
-        bindings: {
-            data: '<'
-        }
-    })
     //choosing the old_questions form
     .component('choosequestion', {
     templateUrl: 'app/old_questions/choosequestion.tpl',
